466e3cbb487f37562493e72262a7b2c22e9511fe
rodge2016 <root@localhost.localdomain>
2018-01-06 08:32:24 +0000

    patch ymm

--- app/code/Mageplaza/LayeredNavigation/Model/ResourceModel/Fulltext/Collection.php
+++ app/code/Mageplaza/LayeredNavigation/Model/ResourceModel/Fulltext/Collection.php
@@ -107,7 +107,11 @@ class Collection extends \Magento\Catalog\Model\ResourceModel\Product\Collection
 		\Magento\Framework\Stdlib\DateTime $dateTime,
 		\Magento\Customer\Api\GroupManagementInterface $groupManagement,
 		\Magento\Framework\Search\Adapter\Mysql\TemporaryStorageFactory $temporaryStorageFactory,
-		\Magento\Framework\DB\Adapter\AdapterInterface $connection = null,
+		\Magento\Framework\Search\Request\Builder $requestBuilder,
+		\Magento\Framework\App\Request\Http $urlInterfaceObj,
+		\Magebees\Finder\Model\Products $finderProduct,
+		\Magebees\Finder\Model\UniversalProduct $universalProduct,
+        \Magento\Framework\DB\Adapter\AdapterInterface $connection = null,
 		$searchRequestName = 'catalog_view_container'
 	)
 	{
@@ -135,6 +139,12 @@ class Collection extends \Magento\Catalog\Model\ResourceModel\Product\Collection
 		);
 		$this->temporaryStorageFactory = $temporaryStorageFactory;
 		$this->searchRequestName       = $searchRequestName;
+		
+		$this->requestBuilder = $requestBuilder;
+        $this->urlInterfaceObj = $urlInterfaceObj;
+		$this->finderProduct = $finderProduct;
+		$this->universalProduct = $universalProduct;
+		$this->scopeConfig = $scopeConfig;
 	}
 
 	/**
@@ -333,11 +343,79 @@ class Collection extends \Magento\Catalog\Model\ResourceModel\Product\Collection
 		return $this;
 	}
 
+	//finder start
+	 public function getFinderProductsSkus($finderStr,$finderId){
+		$product_skus = array();
+		$finderCollection = $this->finderProduct->getCollection()
+									->addFieldToFilter('finder_id',$finderId);
+		
+		$finderArr = array();
+		$finderStr = urldecode($finderStr);
+		$finderArr = explode("-",$finderStr);
+				
+		for($i=0;$i<(count($finderArr));$i++)	{
+			$finderCollection->addFieldToFilter("field".($i+1),$finderArr[$i]);
+		}
+		//print_R($finderCollection->getSelect()->__toString());exit;
+		
+		$product_skus = array_map(array($this,"getProdcutSkusArr"), $finderCollection->getData());
+		
+		return $product_skus;
+	}
+		
+	public function getProdcutSkusArr($element){return $element['sku'];}
+	//finder end
+	
+	
 	/**
 	 * @inheritdoc
 	 */
 	protected function _renderFiltersBefore()
 	{
+		//finder start
+		$path = $this->urlInterfaceObj->getRequestString();
+		$search = strpos($path, 'finder');
+		$skus = array();
+		if($search){
+			$path = trim($path, '/');
+			$finderparams = array();
+			$finderparams = explode('/',$path);
+			
+			if(empty($finderparams[2])){
+				$skus = array('0'=>'');		
+			}else{
+				$fstring = explode("?",$finderparams[2]);
+				$finderparams[2] = $fstring[0];
+				//$finderStr = end($finderparams);
+				$finderStr = $finderparams[2];
+				$finderId = $finderparams[1];
+				$skus = $this->getFinderProductsSkus($finderStr,$finderId);
+
+				//set sort orders
+				$enable_universal = $this->scopeConfig->getValue('finder/general/universal_enable', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+				if($enable_universal){
+					$universal_collection = $this->universalProduct->getCollection()->addFieldToFilter('finder_id',$finderId);
+					$universal_products_skus = array_map(array($this,"getProdcutSkusArr"), $universal_collection->getData());
+					if(!empty($universal_products_skus)){
+						$skus = array_unique(array_merge($skus,$universal_products_skus));
+						$search_skus = array_diff($skus,$universal_products_skus);
+						if(!$this->urlInterfaceObj->getParam('product_list_order')){
+						//Setting Sort order which sort based on the array elements order
+							$sort_order = $this->scopeConfig->getValue('finder/general/sort_order', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+							if($sort_order == 2){
+								$this->getSelect()->order("find_in_set(e.sku,'".implode(',',$universal_products_skus)."')");//display universal products last
+							}elseif($sort_order == 3){
+								$this->getSelect()->order("find_in_set(e.sku,'".implode(',',$search_skus)."')");//display search products last
+							}
+						}
+					}
+				}
+			}
+			$this->requestBuilder->bind('sku', $skus);
+		}
+		
+		//finder end
+		
 		$this->getCollectionClone();
 
 		$this->getSearchCriteriaBuilder();
@@ -386,6 +464,28 @@ class Collection extends \Magento\Catalog\Model\ResourceModel\Product\Collection
 
 		parent::_renderFiltersBefore();
 	}
+		
+	 public function getValues($parentId)
+    {
+        $collection = $this->_ymmvalueFactory->create()->getCollection()->addFieldToFilter('parent_id', $parentId);
+        $data = $collection->getData();
+        if (!$data) {
+            $this->_parent_ids[] = $parentId;
+        }
+           
+       /* if($this->getSort()=='asc') {
+			$order = 'value ASC'; 
+		}else{
+            $order = 'value DESC'; 
+		} */
+        //$collection->getSelect()->order($order);
+        
+        foreach ($collection as $option) {
+            $this->getValues($option->getYmmValueId());
+        }
+    }
+    
+   
 
 	/**
 	 * @return $this
